5653bb466c1f71099e3e7fc5889c4b0bdcb1e4b6
Gropiii <lukas_lp@hotmail.com>
2024-06-17 14:48:09 -0300

    Update app.js

--- Projetos-Estudo/js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ Projetos-Estudo/js-curso-2-aula1/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,22 +1,23 @@
+let listaNumerosSorteados = [];
+let limiteLista = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log (`O número secreto é `, numeroSecreto);
 let tentativas = 1;
 
-function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
-}
-
-function textosDaTela(tag,texto){
+function textosDaTela(tag,texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    //responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1});
 }
-function exibirMensagemInicial() {
- textosDaTela('h1','Jogo do número secreto!');
-textosDaTela('p','Escolha um número de 1 a 10.');       
-}
-exibirMensagemInicial()
 
-function verificarChute() {
+function exibirMensagemInicial() {
+    textosDaTela('h1','Jogo do número secreto!');
+   textosDaTela('p','Escolha um número de 1 a 10.');       
+   }
+
+   exibirMensagemInicial()
+
+   function verificarChute() {
     let chute = document.querySelector('input').value;
     console.log (`O número secreto é `, numeroSecreto);
     console.log ('O chute foi: ', chute);
@@ -24,19 +25,35 @@ function verificarChute() {
     limparCampo();
     if(chute==numeroSecreto){
         textosDaTela('h1','Parabéns, você acertou!');
-        let palavraTentativa = tentativas > 1 ? 'tentivas' : 'tentativa';
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         textosDaTela('p',`Você precisou de ${tentativas} ${palavraTentativa} para descobrir o número secreto (${numeroSecreto}).`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         tentativas++
         if(chute>numeroSecreto) {
-        textosDaTela('h1',`O número secreto é menor que ${chute}`);
-        textosDaTela('p','Tente outro número');
+        textosDaTela('h1',`Você errou.`);
+        textosDaTela('p',`O número secreto é menor que ${chute}`);
     } else{
-        textosDaTela('h1',`O número secreto é maior que ${chute}`);
-        textosDaTela('p','Tente outro número');
+        textosDaTela('h1',`Você errou.`);
+        textosDaTela('p',`O número secreto é maior que ${chute}`);
     }
 }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroAleatorio = parseInt(Math.random() * limiteLista + 1);
+    let quantidadeDeNumeros = listaNumerosSorteados.length;
+
+    if (quantidadeDeNumeros == limiteLista) {
+        listaNumerosSorteados = [];
+    }
+    if (listaNumerosSorteados.includes(numeroAleatorio)){
+        return gerarNumeroAleatorio();
+    } else {
+        console.log(listaNumerosSorteados);
+        listaNumerosSorteados.push(numeroAleatorio);
+        return numeroAleatorio;
+    }
 }
 
 function novoJogo() {
